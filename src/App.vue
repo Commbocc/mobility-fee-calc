45614ccee9542b692bd9c0fb50402a9c7c10b267
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-03-27 09:12:58 -0400

    vue init webpack

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,28 @@
 <template>
-	<div id="app">
-
-		<address-form>
-			<p>
-				{{ address_form_desc }}
-			</p>
-		</address-form>
-
-		<alert v-for="alert in $store.state.alerts" :item="alert" :key="alert.id"></alert>
-
-		<div class="form-group">
-			<label>{{ const_label }}</label>
-			<div class="checkbox">
-				<label>
-					<input type="checkbox" v-model="$store.state.is_new_construction"> {{ const_desc }}
-				</label>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-md-6">
-				<calc-form :n-id="1"
-				title="New Home"></calc-form>
-			</div>
-			<div class="col-md-6">
-				<calc-form :n-id="2"
-				v-if="!$store.state.is_new_construction"
-				title="Existing Home"></calc-form>
-			</div>
-			<div class="col-md-6" :class="offsetResultsClass">
-				<results></results>
-			</div>
-		</div>
-		<!-- <router-view></router-view> -->
-
-	</div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
 </template>
 
 <script>
-import AddressForm from '@/components/AddressForm'
-import CalcForm from '@/components/CalcForm'
-import Results from '@/components/Results'
-import Alert from '@/components/Alert';
+import HelloWorld from './components/HelloWorld'
 
 export default {
-	name: 'index',
-	data () {
-		return {
-			address_form_desc: 'Your address will be used to populate the "Mobility Assessment District" and "Park/Schools Impact Fee Zone" fields below.',
-			const_label: 'New Construction',
-			const_desc: 'This estimate is for a site with no existing home.'
-		}
-	},
-	components: {
-		'address-form': AddressForm,
-		'calc-form': CalcForm,
-		'results': Results,
-		'alert': Alert,
-	},
-	watch: {
-		'$store.state.is_new_construction': function() {
-			this.$store.commit('updateResults')
-		}
-	},
-	computed: {
-		offsetResultsClass () {
-			return (!this.$store.state.is_new_construction) ? 'col-md-offset-3' : null
-		}
-	}
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

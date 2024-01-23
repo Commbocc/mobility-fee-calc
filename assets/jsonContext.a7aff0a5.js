07df0cd124bead15633af687e84ce14fe30c97da
JordanPisani <71664286+JordanPisani@users.noreply.github.com>
2024-01-23 15:40:26 +0000

    Deploying to gh-pages from @ Commbocc/mobility-fee-calc@e3aa2d717778fbfac199cf3ddf53a0ab258b54fb 🚀

---
+++ assets/jsonContext.a7aff0a5.js
@@ -0,0 +1,1 @@
+import{U as t,ag as a}from"./index.069aef39.js";function o(r){return{origin:"portal-item",url:t(r.itemUrl),portal:r.portal||a.getDefault(),portalItem:r,readResourcePaths:[]}}export{o};
